771782cbd1f7951a3aa3f9b460110989a4748f76
Francisco Martinez <panchetz7@gmail.com>
2023-01-19 22:15:46 -0800

    add playercard draft example

---
+++ src/components/PlayerCards/RickieHenderson.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const rickiehenderson = () => {
+  return (
+    <div>
+      <h1>Rickie Henderson</h1>
+      <p>
+        paragraph description of background, nicknames, some things they are
+        known for, etc.
+      </p>
+      <p>bar graph of accolades</p>
+      <p>teams played for</p>
+      <p>
+        youtube video of either highlights or a small segment of some people
+        speaking about the player
+      </p>
+    </div>
+  );
+};
+
+export default rickiehenderson;
